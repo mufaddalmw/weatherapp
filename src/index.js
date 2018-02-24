a2ba636042ef9d5f8979c7927c6fd56d5bd76a76
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2018-02-24 13:43:15 +0400

    Added axis dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import axios from 'axios';
 
-  
-  console.log('hello')
+let apiKey = 'e3d241b4c80e6e87dcaa0979d11b47cc';
+let city = 'dubai';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+// Make a request for a user with a given ID
+axios.get(url)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
