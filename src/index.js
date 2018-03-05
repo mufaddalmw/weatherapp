6518b4f70f0137e69d6201112a29ecc85538a61a
Mufaddal Motorwala <mufaddal.motorwala@rbbideas.com>
2018-03-05 18:23:05 +0400

    added console for individual property

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ let city = 'dubai';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 // Make a request for a user with a given ID
-axios.get(url)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
+axios.get(url).then(function (response) {
+    console.log(response.data);
+    console.log(response.data.coord.lon);
+    console.log(response.data.coord.lon);
+    console.log(response.data.weather[0].description);
+    console.log(response.data.weather[0].icon);
+    console.log(response.data.main.temp - 273.15);
+    console.log(response.data.main.humidity);
+    console.log(response.data.wind.speed);
+    console.log(response.data.sys.country);
+    console.log(response.data.sys.sunrise);
+    console.log(response.data.sys.sunset);
+    console.log(response.data.name);
+
+})
+.catch(function (error) {
     console.log(error);
-  });
+});
